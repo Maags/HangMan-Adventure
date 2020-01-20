2d24b74790e10f92abe1ae46e849ff060388e265
MarcKlesiewicz <marc247hedu.easj.dk>
2020-01-20 14:01:40 +0100

    small visual changes LetterRack function fixed

--- src/UI/LetterRack.java
+++ src/UI/LetterRack.java
@@ -6,30 +6,30 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-/*
+
 public class LetterRack extends JPanel {
 
-    private final int RACK_COLS;
+    private int RACK_COLS;
 
-    private final int RACK_ROWS;
+    private int RACK_ROWS;
 
-    private final GridLayout LETTER_RACK_LAYOUT;
+    private GridLayout LETTER_RACK_LAYOUT;
 
-    private final int CAPACITY;
+    private int CAPACITY;
 
-    private final String IMAGE_DIRECTORY;
+    private String IMAGE_DIRECTORY;
 
-    private final String IMAGE_TYPE;
+    private String IMAGE_TYPE;
 
-    private  final String pickWord;
+    private String password;
 
-    private final ArrayList<LetterTileLoader> rack;
+    private ArrayList<LetterTileLoader> rack;
 
     public LetterRack(){
-        this ("pickWord", "Resources", ".jpeg");
+        this ("password", "Resources", ".jpeg");
     }
 
-    public LetterRack (String inpickWord, String IMAGE_DIRECTORY, String IMAGE_TYPE){
+    public LetterRack (String inPassword, String imageDirectory, String imageType) {
 
 
         RACK_COLS = 13;
@@ -38,27 +38,67 @@ public class LetterRack extends JPanel {
         LETTER_RACK_LAYOUT.setVgap(10);
         CAPACITY = RACK_ROWS * RACK_COLS;
 
-       // IMAGE_DIRECTORY = imageDirectory;
-       // IMAGE_TYPE = imageType;
+        IMAGE_DIRECTORY = imageDirectory;
+        IMAGE_TYPE = imageType;
 
         rack = new ArrayList<>();
-        pickWord = inpickWord;
+        password = inPassword;
 
-        setBorder(BorderFactory.createEmptyBorder(10,17, 10, 10));
+        setBorder(BorderFactory.createEmptyBorder(10, 17, 10, 10));
         BorderFactory.createEmptyBorder();
         setLayout(LETTER_RACK_LAYOUT);
-       // loadRack();
-
-        private void loadRack();{
-          //  buildRack();
-            for (LetterTileLoader tileLoader : rack)
-                add(tileLoader);
-        }
-
-
+        loadRack();
     }
 
 
+    private void loadRack() {
+        buildRack();
+        for (LetterTileLoader tile : rack)
+            add(tile);
+        }
+
+        private void buildRack(){
+            StringBuilder passwordBuilder = new StringBuilder(password.toLowerCase());
+            ArrayList<Character> tiles = new ArrayList<>();
+            Random rand = new Random();
+            int i = 0, j = 0;
+
+            while (passwordBuilder.length() > 0) {
+
+                if (!tiles.contains(passwordBuilder.charAt(0))){
+                    tiles.add(passwordBuilder.charAt(0));
+                    i++;
+                }
+                passwordBuilder.deleteCharAt(0);
+            }
+
+            for (; i < CAPACITY; i++){
+                Character c = 'a';
+                do {
+                    c = (char) (rand.nextInt(26) + 'a');
+                }
+                while (tiles.contains(c));
+                tiles.add(c);
+            }
+
+            for (i = 0; i < CAPACITY; i++) {
+
+                j = rand.nextInt(tiles.size());
+                rack.add(new LetterTileLoader(tiles.get(j), IMAGE_DIRECTORY, IMAGE_TYPE));
+                tiles.remove(j);
+            }
+        }
+
+        public void attachListeners(MouseListener l){
+
+            for (LetterTileLoader tile : rack)
+                tile.addTileListener(l);
+        }
+
+        public void removeListeners(){
+            for (LetterTileLoader tile : rack)
+                tile.removeTileListener();
+        }
 
 
-}*/
+}
